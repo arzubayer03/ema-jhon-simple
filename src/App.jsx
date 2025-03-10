0c66e1d0c70f3c5bdb6dbb98b1d20c68d4db851f
arzubayer03 <arzubayer03@gmail.com>
2025-03-10 16:29:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className='bg-red-200 hover:bg-amber-200'>hello world</h1>
+    </>
+  )
+}
+
+export default App
